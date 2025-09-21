82e280a39dc5fe73153c309de6a1e8f4482a272f
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-09-21 13:53:52 +0500

    Setup NextThemes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ThemeProvider from "@/context/Theme";
 
 const rakkas = localFont({
   src: "./fonts/RakkasVF.ttf",
@@ -29,9 +30,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} ${rakkas.variable} antialiased`}>
-        {children}
+        <ThemeProvider attribute={"class"} enableSystem disableTransitionOnChange defaultTheme="system">{children}</ThemeProvider>
       </body>
     </html>
   );
