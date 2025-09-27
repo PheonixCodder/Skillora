ade952741d3ed8742edb863145120b08cca10544
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-09-27 13:23:53 +0500

    Create Interaction Model

---
+++ database/interaction.model.ts
@@ -0,0 +1,41 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export const InteractionActionEnums = [
+  "view",
+  "upvote",
+  "downvote",
+  "bookmark",
+  "post",
+  "edit",
+  "delete",
+  "search",
+] as const;
+
+export interface IInteraction {
+  user: Types.ObjectId;
+  action: (typeof InteractionActionEnums)[number];
+  actionId: Types.ObjectId;
+  actionType: "question" | "answer";
+}
+
+const InteractionSchema = new Schema<IInteraction>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    action: {
+      type: String,
+      enum: InteractionActionEnums,
+      required: true,
+    },
+    actionId: { type: Schema.Types.ObjectId, required: true },
+    actionType: {
+      type: String,
+      enum: ["question", "answer"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Interaction = models?.Interaction || model<IInteraction>("Interaction", InteractionSchema);
+
+export default Interaction;
