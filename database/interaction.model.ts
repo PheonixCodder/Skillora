69c86cd2a49a20ec4c52634e9c5bd7ff6efd67b6
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-02 22:27:53 +0500

    Update interaction.model.ts

--- database/interaction.model.ts
+++ database/interaction.model.ts
@@ -1,4 +1,11 @@
-import { model, models, Schema, Types } from "mongoose";
+import { Schema, models, model, Types, Document } from "mongoose";
+
+export interface IInteraction {
+  user: Types.ObjectId;
+  action: string;
+  actionId: Types.ObjectId;
+  actionType: string;
+}
 
 export const InteractionActionEnums = [
   "view",
@@ -11,13 +18,7 @@ export const InteractionActionEnums = [
   "search",
 ] as const;
 
-export interface IInteraction {
-  user: Types.ObjectId;
-  action: (typeof InteractionActionEnums)[number];
-  actionId: Types.ObjectId;
-  actionType: "question" | "answer";
-}
-
+export interface IInteractionDoc extends IInteraction, Document {}
 const InteractionSchema = new Schema<IInteraction>(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
@@ -26,16 +27,13 @@ const InteractionSchema = new Schema<IInteraction>(
       enum: InteractionActionEnums,
       required: true,
     },
-    actionId: { type: Schema.Types.ObjectId, required: true },
-    actionType: {
-      type: String,
-      enum: ["question", "answer"],
-      required: true,
-    },
+    actionId: { type: Schema.Types.ObjectId, required: true }, // 'questionId', 'answerId',
+    actionType: { type: String, enum: ["question", "answer"], required: true },
   },
   { timestamps: true }
 );
 
-const Interaction = models?.Interaction || model<IInteraction>("Interaction", InteractionSchema);
+const Interaction =
+  models?.Interaction || model<IInteraction>("Interaction", InteractionSchema);
 
 export default Interaction;
