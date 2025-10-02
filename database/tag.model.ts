0236596f7bd7f1765a98d4a59ddcf5dc1cebf9ec
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-02 22:27:42 +0500

    Update tag.model.ts

--- database/tag.model.ts
+++ database/tag.model.ts
@@ -1,13 +1,12 @@
-import { Document, model, models, Schema } from "mongoose";
+import { model, models, Schema, Document } from "mongoose";
 
 export interface ITag {
   name: string;
   questions: number;
 }
 
-export type ITagDoc = ITag & Document;
-
-const TagSchema = new Schema<ITagDoc>(
+export interface ITagDoc extends ITag, Document {}
+const TagSchema = new Schema<ITag>(
   {
     name: { type: String, required: true, unique: true },
     questions: { type: Number, default: 0 },
