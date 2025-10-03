3a8dcf62811976dac84e014e499eabbd0dccac92
Abdulahad <212123310+PheonixCodder@users.noreply.github.com>
2025-10-03 19:21:32 +0500

    Created Tags Id page

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,56 @@
+import QuestionCard from "@/components/layout/cards/QuestionCard";
+import Pagination from "@/components/layout/Pagination";
+import LocalSearch from "@/components/layout/search/LocalSearch";
+import DataRenderer from "@/components/ui/DataRenderer";
+import { ROUTES } from "@/constants/routes";
+import { STATES } from "@/constants/states";
+import { getTagQuestions } from "@/lib/actions/tag.action";
+
+const Page = async ({ params, searchParams }: RouteParams) => {
+  const { id } = await params;
+  const { page, pageSize, query } = await searchParams;
+
+  const { success, data, error } = await getTagQuestions({
+    tagId: id,
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query,
+  });
+
+  const { tag, questions, isNext } = data || {};
+
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">{tag?.name}</h1>
+      </section>
+
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.TAG(id)}
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          className="flex-1"
+        />
+      </section>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={questions}
+        empty={STATES.EMPTY_QUESTION}
+        render={(questions) => (
+          <div className="mt-10 flex w-full flex-col gap-6">
+            {questions.map((question) => (
+              <QuestionCard key={question._id} question={question} />
+            ))}
+          </div>
+        )}
+      />
+
+      <Pagination page={page} isNext={isNext || false} />
+    </>
+  );
+};
+
+export default Page;
